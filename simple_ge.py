565c962f05d3f7c60a226aa2401e754d2362e6df
LegenDUST <dhnam0502@naver.com>
2022-11-16 00:01:52 +0900

    misc. bug fix

--- simple_ge.py
+++ simple_ge.py
@@ -16,7 +16,7 @@ class Environment:
         for (strt1_idx, strt1), (strt2_idx, strt2) in combinations_with_replacement(enumerate(self.population), 2):
             strt1_final_reward = 0
             strt2_final_reward = 0
-            for _, (reward1, reward2) in Duel(strt1, strt2):
+            for _, (reward1, reward2) in Duel(strt1, strt2, REWARD_TABLE, DUEL_LENGTH):
                 strt1_final_reward = reward1
                 strt2_final_reward = reward2
             self.scores[strt1_idx] = strt1_final_reward
