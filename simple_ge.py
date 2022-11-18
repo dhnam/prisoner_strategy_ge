3bc470660cf6370b4312259d67875e83d60c7937
LegenDUST <dhnam0502@naver.com>
2022-11-18 22:07:37 +0900

    Next generation

--- simple_ge.py
+++ simple_ge.py
@@ -2,15 +2,21 @@ from strategy.basic_config import *
 from strategy.strategy import Strategy
 from strategy.duel import Duel
 from itertools import combinations_with_replacement
+import random
 
 class Environment:
     def __init__(self, size: int):
+        self._size: int = size
         self.population: list[Strategy] = []
         for next_population in range(size):
             self.population.append(Strategy(f"Gen0_{next_population}"))
 
         self.scores: list[float] = [0] * size
         self.generation: int = 0
+
+    @property
+    def size(self) -> int:
+        return self._size
 
     def generation_processing(self):
         for (strt1_idx, strt1), (strt2_idx, strt2) in combinations_with_replacement(enumerate(self.population), 2):
@@ -22,9 +28,14 @@ class Environment:
             self.scores[strt1_idx] = strt1_final_reward
             self.scores[strt2_idx] = strt2_final_reward
 
-    def new_generation(self):
-        # stratage: 50% -> last gen, 50%: percentage-based
-        self.scores = [0] * size
+    def next_generation(self):
+        # stratage: 100%: percentage-based
+        parents = random.choices(self.population, self.scores, k=len(self.size))
+        self.population = []
+        for next_parent in parents:
+            self.population.append(next_parent.clone().mutate(MUTATE_RATE))
+
+        self.scores = [0] * self.size
 
 
 
@@ -35,11 +46,14 @@ if __name__ == "__main__":
     population = int(input("Input population: "))
     env = Environment(population)
     gen = 0
+    env.generation_processing()
     while True:
         # Menu: process, show top, ...?
         print(f"====GENERATION {gen}====")
         menu = input("Process: p, Show nth: (input number), Show score: s")
         if menu == "p":
+            gen += 1
+            env.next_generation()
             env.generation_processing()
         if menu.isdigit() and int(menu) < population:
             print(self.population[int(menu)])
