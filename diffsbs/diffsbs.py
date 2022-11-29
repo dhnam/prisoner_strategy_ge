3dffa0c60cf634473fac820ea02ac0e7e009377b
LegenDUST <dhnam0502@naver.com>
2022-11-29 15:05:01 +0000

    Almost finish sbs diff... Test it later

--- diffsbs/diffsbs.py
+++ diffsbs/diffsbs.py
@@ -7,15 +7,15 @@ class Diffsbs(difflib.Differ):
     def __init__(self, linejunk: Callable[[str], bool] | None=None, charjunk: Callable[[str], bool]=None):
         super().__init__(linejunk, charjunk)
         self._maxlen: int = -1
+        self._recent_indicator: str = ''
+        self._recent_lines: list[str] = []
         #todo add things...
 
-    def comparesbs(self, a: Sequence[str], b: Sequence[str]) -> Iterator[str]:
+    def comparesbs(self, a: Sequence[str], b: Sequence[str], sep="") -> Iterator[str]:
         # Copy-pasted code from cpython difflib
 
         # First we need to know maximum length to make sbs compare pretty.
-        for next_len in map(len, chain(a, b)):
-            if next_len > self._maxlen:
-                self._maxlen = next_len
+        self._maxlen = max(map(len, chain(a, b))) + 2
 
         cruncher = difflib.SequenceMatcher(self.linejunk, a, b)
         for tag, alo, ahi, blo, bhi in cruncher.get_opcodes():
@@ -29,124 +29,103 @@ class Diffsbs(difflib.Differ):
                 g = self._dump(' ', a, alo, ahi)
             else:
                 raise ValueError('unknown tag %r' % (tag,))
-            g = self._build_sbs(tag, g, sep='')
+            g = self._build_sbs(tag, g, sep=sep)
 
             yield from g
 
     def _build_sbs(self, tag: str, lines: Iterator[str], sep='') -> Iterator[str]:
-        def join_with_spaces(a: str, b: str, space: int, sep=''):
-            return ''.join([a[:-1].ljust(space), sep, b[:-1].ljust(space), "\n"])
-        a = ""
-        a_replace = ""
-        b = ""
-        b_replace = ""
-        was = ""
-        replaced = False
-        # print(list(lines))
+        def join_with_spaces(a: str, b: str, space: int, sep='') -> str:
+            return ''.join([a.ljust(space), sep, b.ljust(space), "\n"])
+
+        def new_line_start(*, a: str="", b: str="", a_replace: str | None=None, b_replace: str | None=None) -> Iterator[str]:
+            yield join_with_spaces(a, b, self._maxlen, sep)
+            if any([a_replace, b_replace]):
+                yield join_with_spaces("" if a_replace is None else a_replace,
+                                       "" if b_replace is None else b_replace,
+                                        self._maxlen, sep)
+        # new line patterns: " ", "-", "+", "-?+", "-+?", "-?+?"
+        # tokens: " ", "-", "+", "?"
         for next_line in lines:
-            # print(next_line[:-1], a[:-1], b[:-1], a_replace[:-1], b_replace[:-1], sep=", ")
-            if next_line[:2] == "- ":
-                match was:
-                    case "- ":
-                        b = "\n"
-                        yield join_with_spaces(a, b, self._maxlen, sep)
-                        replaced = False
-                    case "+ ":
-                        a = "\n"
-                        yield join_with_spaces(a, b, self._maxlen, sep)
-                        replaced = False
-                    case "? ":
-                        yield join_with_spaces(a, b, self._maxlen, sep)
-                        yield join_with_spaces(a_replace, b_replace, self._maxlen, sep)
-                        replaced = False
-                    case "  ":
-                        yield join_with_spaces(a, b, self._maxlen, sep)
-                        replaced = False
-                a = next_line
-                was = "- "
-            if next_line[:2] == "+ ":
-                match was:
-                    case "- ":
-                        b = "\n"
-                        yield join_with_spaces(a, b, self._maxlen, sep)
-                        replaced = False
-                    case "+ ":
-                        if replaced:
-                            b_replace = "\n"
-                            yield join_with_spaces(a, b, self._maxlen, sep)
-                            yield join_with_spaces(a_replace, b_replace, self._maxlen, sep)
-                            replaced = False
-                        else:
-                            a = "\n"
-                            yield join_with_spaces(a, b, self._maxlen, sep)
-                            replaced = False
-                b = next_line
-                was = "+ "
-            if next_line[:2] == "? ":
-                match was:
-                    case "- ":
-                        a_replace = next_line
-                    case "+ ":
-                        b_replace = next_line
-                replaced = True
-                was = "? "
-            if next_line[:2] == "  ":
-                match was:
-                    case "- ":
-                        b = "\n"
-                        yield join_with_spaces(a, b, self._maxlen, sep)
-                        replaced = False
-                    case "+ ":
-                        a = "\n"
-                        yield join_with_spaces(a, b, self._maxlen, sep)
-                        replaced = False
-                    case "? ":
-                        print("Haha sorry...")
-                yield join_with_spaces(next_line, next_line, self._maxlen, sep)
-                was = "  "
-                replaced = False
-        """ 
-        match was:
+            next_line = next_line.strip("\n")
+            self._recent_indicator += next_line[0]
+            self._recent_lines.append(next_line)
+            match self._recent_indicator:
+                case " ":
+                    yield from new_line_start(a=next_line, b=next_line)
+                    self._recent_indicator = ''
+                    self._recent_lines = []
+                case "+":
+                    yield from new_line_start(b=next_line)
+                    self._recent_indicator = ''
+                    self._recent_lines = []
                 case "- ":
-                case "+ ":
-                case "? ":
-                case "  ":
-                case "":
-                case _:
-        """
-
-        # TODO finish 'last token'
-
-        """
-        if tag == 'replace':
-            
-            a = next(lines)
-            a_replace = next(lines)
-            try:
-                b = next(lines)
-                b_replace = next(lines)
-                yield join_with_spaces(a, b, self._maxlen, sep)
-                yield join_with_spaces(a_replace, b_replace, self._maxlen, sep)
-            except StopIteration:
-                yield join_with_spaces(a, a_replace, self._maxlen, sep)
-        elif tag == 'delete':
-            a = next(lines)
-            b = "\n"
-            yield join_with_spaces(a, b, self._maxlen, sep)
-        elif tag == 'insert':
-            a = "\n"
-            b = next(lines)
-            yield join_with_spaces(a, b, self._maxlen, sep)
-        elif tag == 'equal':
-            a = next(lines)
-            b = a
-            yield join_with_spaces(a, b, self._maxlen, sep)
-        else:
-            raise ValueError('unknown tag %r' % (tag,))
-        """
-
-
+                    yield from new_line_start(a=self._recent_lines[0])
+                    yield from new_line_start(a=next_line, b=next_line)
+                    self._recent_indicator = ''
+                    self._recent_lines = []
+                case "--":
+                    yield from new_line_start(a=self._recent_lines[0])
+                    self._recent_indicator = "-"
+                    self._recent_lines.pop(0)
+                case "-+ ":
+                    yield from new_line_start(a=self._recent_lines[0])
+                    yield from new_line_start(b=self._recent_lines[1])
+                    yield from new_line_start(a=next_line, b=next_line)
+                    self._recent_indicator = ''
+                    self._recent_lines = []
+                case "-+-":
+                    yield from new_line_start(a=self._recent_lines[0])
+                    yield from new_line_start(b=self._recent_lines[1])
+                    self._recent_indicator = '-'
+                    self._recent_lines = self._recent_lines[2:]
+                case "-++":
+                    yield from new_line_start(a=self._recent_lines[0])
+                    yield from new_line_start(b=self._recent_lines[1])
+                    yield from new_line_start(b=next_line)
+                    self._recent_indicator = ''
+                    self._recent_lines = []
+                case "-+?":
+                    yield from new_line_start(a=self._recent_lines[0], b=self._recent_lines[1], b_replace=self._recent_lines[2])
+                    self._recent_indicator = ''
+                    self._recent_lines = []
+                case "-? " | "-?-" | "-??":
+                    raise Exception(self._recent_indicator)
+                case "-?+ ":
+                    yield from new_line_start(a=self._recent_lines[0], b=self._recent_lines[2], a_replace=self._recent_lines[1])
+                    yield from new_line_start(a=next_line, b=next_line)
+                    self._recent_indicator = ''
+                    self._recent_lines = []
+                case "-?+-":
+                    yield from new_line_start(a=self._recent_lines[0], b=self._recent_lines[2], a_replace=self._recent_lines[1])
+                    self._recent_indicator = '-'
+                    self._recent_lines = self._recent_lines[3:]
+                case "-?++":
+                    yield from new_line_start(a=self._recent_lines[0], b=self._recent_lines[2], a_replace=self._recent_lines[1])
+                    yield from new_line_start(b=next_line)
+                    self._recent_indicator = ''
+                    self._recent_lines = []
+                case "-?+?":
+                    yield from new_line_start(a=self._recent_lines[0], b=self._recent_lines[2], a_replace=self._recent_lines[1], b_replace=self._recent_lines[3])
+                    self._recent_indicator = ''
+                    self._recent_lines = []
+        
 if __name__ == "__main__":
-     print(''.join(Diffsbs().comparesbs('one\ntwo\nthree\nfour\nsix\n'.splitlines(True),
-                                    'ore\ntree\nemu\nfour\nfive\n'.splitlines(True))),
+    a = """First
+second
+third
+fourth
+fifth
+seventh"""
+    b = """Firstly
+thiid
+forth
+fivth
+sixth
+seventh"""
+    print(''.join(difflib.Differ().compare(a.splitlines(True),
+                                        b.splitlines(True))),
+                    end="")
+    print()
+    print(''.join(Diffsbs().comparesbs(a.splitlines(True),
+                                        b.splitlines(True), sep="|")),
                     end="")
