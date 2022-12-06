ddea4f5491fb5c0a83708b3d383bde984c8cbfd0
LegenDUST <dhnam0502@naver.com>
2022-12-06 14:43:59 +0000

    Side by side maybe completed

--- diffsxs/diffsxs.py
+++ diffsxs/diffsxs.py
@@ -1,16 +1,15 @@
 import difflib
 from typing import Sequence, Iterator
 from abc import ABC, abstractmethod
-
-# TODO yak shaving for unicode characer && zero length character... how to calculate it && apply it in join_with_spaces
+from wcwidth import wcswidth, wcwidth
 
 class SxsStratagy(ABC):
-    def __init__(self, sep=""):
+    def __init__(self, sep="|"):
         self.maxlen = -1
         self.sep = sep
     
     def join_with_spaces(self, a: str, b: str) -> str:
-        return f"{a:<{self.maxlen}}{self.sep}{b:<{self.maxlen}}" + "\n"
+        return f"{a}{' '*(self.maxlen - wcswidth(a))}{self.sep}{b}" + "\n"
 
     @abstractmethod
     def next_line(self, *, a: str="", b: str="", a_replace: str | None=None, b_replace: str | None=None) -> Iterator[str]:
@@ -18,11 +17,19 @@ class SxsStratagy(ABC):
 
 class SimpleSxsStratagy(SxsStratagy):
     def next_line(self, *, a: str="", b: str="", a_replace: str | None=None, b_replace: str | None=None) -> Iterator[str]:
+        def replace_unicode_match(string: str, replace: str) -> str:
+            ret = ""
+            for next_char, next_replace in zip(string, replace):
+                if wcwidth(next_char) == 1:
+                    ret += next_replace
+                elif wcwidth(next_char) == 2:
+                    ret += next_replace * 2
+            return ret
         yield self.join_with_spaces(a, b)
         if any([a_replace, b_replace]):
             yield self.join_with_spaces(
-                "" if a_replace is None else a_replace,
-                "" if b_replace is None else b_replace,
+                "" if a_replace is None else replace_unicode_match(a, a_replace),
+                "" if b_replace is None else replace_unicode_match(b, b_replace),
                 )
 
 
@@ -64,9 +71,6 @@ class UnicodeOnelineSxsStratagy(SxsStratagy):
         yield self.join_with_spaces(*processed)
 
 
-        
-
-
 class Diffsxs:
     def __init__(self, sxs_stratagy: SxsStratagy | None=None):
         self._recent_indicator: str = ''
@@ -75,8 +79,8 @@ class Diffsxs:
             sxs_stratagy = SimpleSxsStratagy()
         self.sxs_stratagy = sxs_stratagy
 
-    def comparesbs(self, a: Sequence[str], b: Sequence[str]) -> Iterator[str]:
-        self.sxs_stratagy.maxlen = max(map(len, a)) + 2
+    def comparesxs(self, a: Sequence[str], b: Sequence[str]) -> Iterator[str]:
+        self.sxs_stratagy.maxlen = max(map(wcswidth, map(str.strip, a))) + 2
         lines = difflib.Differ().compare(a, b)
         # new line patterns: " ", "-", "+", "-?+", "-+?", "-?+?"
         # tokens: " ", "-", "+", "?"
@@ -84,6 +88,7 @@ class Diffsxs:
             next_line = next_line.strip("\n")
             self._recent_indicator += next_line[0]
             self._recent_lines.append(next_line)
+            
             match self._recent_indicator:
                 case " ":
                     yield from self.sxs_stratagy.next_line(a=next_line, b=next_line)
@@ -143,6 +148,32 @@ class Diffsxs:
                     yield from self.sxs_stratagy.next_line(a=self._recent_lines[0], b=self._recent_lines[2], a_replace=self._recent_lines[1], b_replace=self._recent_lines[3])
                     self._recent_indicator = ''
                     self._recent_lines = []
+        match self._recent_indicator:
+            case " ":
+                yield from self.sxs_stratagy.next_line(a=self._recent_lines[0], b=self._recent_lines[0])
+                self._recent_indicator = ''
+                self._recent_lines = []
+            case "+":
+                yield from self.sxs_stratagy.next_line(b=self._recent_lines[0])
+                self._recent_indicator = ''
+                self._recent_lines = []
+            case "-":
+                yield from self.sxs_stratagy.next_line(a=self._recent_lines[0])
+                self._recent_indicator = ''
+                self._recent_lines = []
+            case "-?+":
+                yield from self.sxs_stratagy.next_line(a=self._recent_lines[0], b=self._recent_lines[2], a_replace=self._recent_lines[1])
+                self._recent_indicator = ''
+                self._recent_lines = []
+            case "-+?":
+                yield from self.sxs_stratagy.next_line(a=self._recent_lines[0], b=self._recent_lines[1], b_replace=self._recent_lines[2])
+                self._recent_indicator = ''
+                self._recent_lines = []
+            case "-?+?":
+                yield from self.sxs_stratagy.next_line(a=self._recent_lines[0], b=self._recent_lines[2], a_replace=self._recent_lines[1], b_replace=self._recent_lines[3])
+                self._recent_indicator = ''
+                self._recent_lines = []
+            
         
 if __name__ == "__main__":
     a = """First
@@ -150,18 +181,26 @@ second
 third
 fourth
 fifth
-seventh"""
+seventh
+나는 자랑스런 태극기 앞에
+조국과 민족의
+무궁한 영광을 위하여 몸과 마음을 바쳐 충성을 다할 것을
+굳게 다짐합니다"""
     b = """Fierstly
 thiid
 forth
 fivth
 sixth
-seventh"""
+seventh
+나는 자랑스러운 태극기 앞에
+자유롭고 정의로운 대한민국의
+무궁한 영광을 위하여 충성을 다할 것을
+굳게 다짐합니다"""
     print(''.join(difflib.Differ().compare(a.splitlines(True),
-                                        b.splitlines(True))),
-                    end="")
+                                        b.splitlines(True))))
+    print(list(map(wcswidth, a.splitlines(True))))
     print()
-    sxs_stratagy = UnicodeOnelineSxsStratagy(sep="|")
-    print(''.join(Diffsxs(sxs_stratagy=sxs_stratagy).comparesbs(a.splitlines(True),
+    sxs_stratagy = SimpleSxsStratagy(sep="|")
+    print(''.join(Diffsxs(sxs_stratagy=sxs_stratagy).comparesxs(a.splitlines(True),
                                                                 b.splitlines(True))),
                     end="")
